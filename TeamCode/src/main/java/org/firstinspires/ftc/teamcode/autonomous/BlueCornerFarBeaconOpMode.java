1295240ff1352a9f12e16b318c9a657259ec9096
unholydeath785 <evan@coulson-orellana.com>
2016-10-25 13:01:19 -0700

    New Class, such as Flicker and new autonomous op mode to clarify naming convention

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueCornerFarBeaconOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+/**
+ * Created by EvanCoulson on 10/23/16.
+ */
+public class BlueCornerFarBeaconOpMode extends AutonomousOpMode {
+    BlueCornerFarBeaconOpMode() {
+        super.initializeAllDevices();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}

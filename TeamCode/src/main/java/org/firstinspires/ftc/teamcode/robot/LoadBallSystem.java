bb804d066271999a84dc6ee42b25d0d0a5b0c9a2
Jack Shew <jack@shew.net>
2016-10-31 16:46:11 -0700

    Added Systems and motors for robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/LoadBallSystem.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+/**
+ * Created by jacks on 10/31/2016.
+ */
+
+
+/*
+Loading the Ball into Flicker from loading platform:
+    In Autonomous:
+        Wait for first ball to shoot, pause motor.
+        Put Angle Adjuster at LOAD position
+        Lift servo to release second ball
+        Wait, then lift Angle Adjuster to desired shot position
+        Shoot
+
+    In Tele-op:
+        With the press of a button, raise the BallHolderServo
+
+
+
+
+
+
+
+
+*/
+public class LoadBallSystem {
+    public LoadBallSystem() {
+
+    }
+}

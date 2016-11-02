eda47f05217a4b4e583020bbc0b07e3fff1a41d7
unholydeath785 <evan@coulson-orellana.com>
2016-11-02 15:26:50 -0700

    Structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueCenterVortexNearBeaconOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+/**
+ * Created by EvanCoulson on 10/23/16.
+ */
+public class BlueCenterVortexNearBeaconOpMode extends AutonomousOpMode {
+    BlueCenterVortexNearBeaconOpMode() {
+        super.initializeAllDevices();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
